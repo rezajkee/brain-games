3718c16ffd8cd443990c853bf35c261779e607d2
Anton Shefer <rezajkee@mail.ru>
2022-04-09 14:37:14 +0500

    refactoring engine and prime logic

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -8,15 +8,16 @@ def start_the_game(get_question_and_answer, DESCRIPTION):
     print('Welcome to the Brain Games!')
     username = prompt.string('May I have your name? ')
     print(f'Hello, {username}!')
-    win_streak = 0
+    points_to_win = 3
+    points_streak = 0
     print(DESCRIPTION)
-    while win_streak < 3:
+    while points_streak < points_to_win:
         right_answer, question = get_question_and_answer()
         print(f'Question: {question}')
         user_answer = prompt.string('Your answer: ')
         if right_answer == user_answer:
             print('Correct!')
-            win_streak += 1
+            points_streak += 1
         else:
             print(f"'{user_answer}' is wrong answer ;(."
                   f" Correct answer was '{right_answer}'.")
--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,19 +7,14 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(question):
-    check_index1 = 2
-    check_index2 = 0
-
-    while check_index1 * check_index1 <= question and check_index2 != 1:
-        if question % check_index1 == 0:
-            check_index2 = 1
-        check_index1 += 1
-    if check_index2 == 1:
+    if question == 1:
         return False
-    elif question == 1:
-        return False
-    else:
-        return True
+    if question % 2 == 0:
+        return question == 2
+    check_index = 3
+    while check_index * check_index <= question and question % check_index != 0:
+        check_index += 2
+    return check_index * check_index > question
 
 
 def get_question_and_answer():
